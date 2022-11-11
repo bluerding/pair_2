21e572c6666f37720648bd88ab85c544380be187
Ben Luerding <bluerding@gmail.com>
2022-11-11 15:48:23 -0600

    Add cities

--- friends.js
+++ friends.js
@@ -2,3 +2,8 @@ var arrays = ["Kerri", "Whitney", "Nick", "Martina", "Maya"]
 console.log(arrays[2])
 console.log(arrays[4])
 console.log(arrays[0])
+
+var places = ["Rome", "Paris", "Milan", "Berlin", "Goa"]
+places.pop()
+console.log(places)
+console.log(places.length)
